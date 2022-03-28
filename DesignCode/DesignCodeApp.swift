2309c0ad384c2365440ed12b9daa840130bde1a7
Tiso <tisobyn@gmail.com>
2022-03-28 16:40:02 +0600

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Yermek Sabyrzhan on 28.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
