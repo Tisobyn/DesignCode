71344d87ca792b4a91e4f30582fdf90b8c955407
Tiso <tisobyn@gmail.com>
2022-04-13 17:14:12 +0600

    feat: Scrolling 3D collection and animation

--- DesignCode/Scenes/Views/Home.swift
+++ DesignCode/Scenes/Views/Home.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Home: View {
     @Binding var showProfile: Bool
+    @State var showUpdate = false
 
     var body: some View {
         VStack {
@@ -16,9 +17,21 @@ struct Home: View {
                 Text("Watching")
                     .font(.system(size: 28, weight: .bold))
                 Spacer()
-                ZStack {
-                    AvatarView(showProfile: $showProfile)
+
+                AvatarView(showProfile: $showProfile)
+
+                Button(action: {
+                    self.showUpdate.toggle()
+                }) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
                 }
+                .sheet(isPresented: $showUpdate) {
+                    
+                }
+
             }
             .padding(.horizontal)
             .padding(.leading, 14)
@@ -29,8 +42,21 @@ struct Home: View {
                 showsIndicators: false) {
 
                 HStack(spacing: 30) {
-                    ForEach(0 ..< 5) { item in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(
+                                    Angle(
+                                        degrees: geometry.frame(
+                                            in: .global
+                                        ).minX / 20
+                                    ),
+                                    axis: (
+                                        x: 0, y: 10, z: 0
+                                    )
+                                )
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
                 .padding(30)
@@ -49,10 +75,12 @@ struct Home_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype design for SwiftUI")
+                Text(section.title)
                     .font(.system(
                         size: 24,
                         weight: .bold))
@@ -61,15 +89,15 @@ struct SectionView: View {
                         alignment: .leading)
                     .foregroundColor(.white)
                 Spacer()
-                Image("Logo1")
+                Image(section.logo)
             }
 
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(
                     maxWidth: .infinity,
                     alignment: .leading)
 
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -77,10 +105,43 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
         .shadow(
-            color: Color("card1").opacity(0.3),
+            color: section.color.opacity(0.3),
             radius: 20, x: 0, y: 20)
     }
 }
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(
+        title: "Prototype design for SwiftUI",
+        text: "18 Sections",
+        logo: "Logo1",
+        image: Image("Card1"),
+        color: Color("card1")
+    ),
+    Section(
+        title: "Build a SwiftUI app",
+        text: "20 Sections",
+        logo: "Logo1",
+        image: Image("Card2"),
+        color: Color("card2")
+    ),
+    Section(
+        title: "SwiftUI Advance",
+        text: "20 Sections",
+        logo: "Logo1",
+        image: Image("Card3"),
+        color: Color("card3")
+    )
+]
